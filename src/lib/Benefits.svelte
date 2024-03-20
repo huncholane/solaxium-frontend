dfccf254391f7cf87e52e3ea448f5c1de05a2d77
I-Am-Finite <resercmichael@gmail.com>
2024-03-20 11:13:23 -0500

    solaxium

--- src/lib/Benefits.svelte
+++ src/lib/Benefits.svelte
@@ -37,45 +37,46 @@
 			{#if tabSet === 0}
 				<ol>
 					<li>
-						Solana has the foundation to be truly great, however the dex platforms cause issues when
-						placing orders effectively. The available platforms are slow and buggy.
+						Solana paved the foundation for a truly great market, however the dex platforms cause issues when
+						placing orders & the available platforms are slow and buggy.
 					</li>
 					<li>
-						The Solana web3 sdks don't help very much, especially when it comes to making efficient
-						Solana automations. It's difficult to and sometimes impossible to track transactions on
-						the javascript SDK.
+						The Solana Web3 SDKS don't offer much help when the documentation for it is severely outdated, especially when it comes to making efficient
+						Solana automations. Right now it's difficult to, and sometimes impossible to track transactions on
+						the JavaScript SDK.
 					</li>
 					<li>
-						The Rust sdk works much better, but the documentation lacks many examples and creates
-						large complexities, especially for new developers.
+						The Rust SDK works much better out of the box, but the documentation lacks the robust support some chains boast and that creates
+						many complexities, especially for new developers.
 					</li>
 				</ol>
 			{:else if tabSet === 1}
 				<ol>
 					<li>
-						When one interacts with Jupiter or Raydium there is a good chance the user will run into
-						bugs in the first 30 seconds.
+						When a user interfaces with either the Jupiter or Raydium exchanges, there is a good chance the they will run into
+						bugs in the offset if their user experiences. This leads to failed trades and poor visualization of profit and loss margins.
 					</li>
 					<li>
-						We launched our community token monkewifhat ($MIF) and ran into difficulties interacting
-						with the Raydium AMM generator. We had to switch to a new platform.
+						Earlier this year, we launched our community token MonkeWifHat ($MIF) and ran into many difficulties interacting
+						with the Raydium AMM generator. We knew then we had to switch to a new platform, but which one?
+						With no solution easily available, we set out to make our own.
 					</li>
 					<li>
-						We enjoy the Bonk bot as it simplifies the process of placing orders on the Raydium
-						exchange. Shout out to the Bonk devs for the inspiration of Sollaxium.
+						We enjoy the Bonk Telegram Trading bot as it simplifies the process of placing orders on the Raydium
+						exchange, and speeds up the rate at which information is available to its users. Shout out to the Bonk devs largely inspriring this project.
 					</li>
 				</ol>
 			{:else if tabSet === 2}
 				<ol>
-					<li>Sollaxium is here to simplify trading with a much more efficient solution.</li>
+					<li>Finally! Solaxium, the fastest de-centralized, Solana-based, utility-driven exchange is here to simplify trading with a much more efficient solution!</li>
 					<li>
-						Our team wants to write a backend in the efficient language Rust, which is what Solana
-						is based on. We all know how inefficient JavaScript is, especially for something as big
-						as Solana.
+					 	We all know how inefficient JavaScript is (Devs that is), especially for something as big, scalable, and publiclly available
+						as Solana. We say no more!
 					</li>
 					<li>
-						We will create a simplified Rust cli and grow it over time towards a web user interface.
-						Our plans on how to do so will remain private to the Sollaxium company.
+						Our team will create an efficient and simplified Rust library to replace the clunky, old Solana CLI and use it to build both an exchange platform
+						and lightning-fast utilities.
+						"What's next then?", you ask? Wouldn't you like to know? 
 					</li>
 				</ol>
 			{/if}
