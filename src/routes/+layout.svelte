0c434934991ef04db682441bb383396f52227bcc
I-Am-Finite <resercmichael@gmail.com>
2024-03-20 09:09:19 -0500

    solaxium

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -44,27 +44,11 @@
 			<svelte:fragment slot="trail">
 				<a
 					class="btn btn-sm variant-ghost-surface"
-					href="https://discord.gg/SxswvkgfKn"
+					href="https://github.com/huncholane/solaxium/blob/main/ReadMe.md"
 					target="_blank"
 					rel="noreferrer"
 				>
-					Discord
-				</a>
-				<a
-					class="btn btn-sm variant-ghost-surface"
-					href="https://twitter.com/Solaxium"
-					target="_blank"
-					rel="noreferrer"
-				>
-					Twitter
-				</a>
-				<a
-					class="btn btn-sm variant-ghost-surface"
-					href="https://github.com/huncholane/solaxium"
-					target="_blank"
-					rel="noreferrer"
-				>
-					GitHub
+					WhitePaper
 				</a>
 			</svelte:fragment>
 		</AppBar>
